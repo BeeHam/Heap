cfeebf84c696e06eabe6e4805bb9498a8bcbcfdd
Brigham <brigham.thompson@gmail.com>
2015-11-09 16:38:48 -0800

    add new seed content for questions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Question.create(question_title: "Where a test question?", question_content: "Where a test question?", question_author: "Jill Doe")
-Question.create(question_title: "When a test question?", question_content: "When a test question?", question_author: "Jim Doe")
-Question.create(question_title: "Why a test question?", question_content: "Why a test question?", question_author: "John Doe")
-Question.create(question_title: "How a test question?", question_content: "How a test question?", question_author: "Jane Doe")
+Question.create(question_title: "Where a test question?", question_content: "A bird is an instrument working according to mathematical law, which instrument it is within the capacity of man to reproduce with all its movements.
+
+Thank God men cannot fly, and lay waste the sky as well as the earth.
+
+I suppose we shall soon travel by air-vessels; make air instead of sea voyages; and at length find our way to the Moon, in spite of the want of atmosphere.", question_author: "Jill Doe")
+Question.create(question_title: "When a test question?", question_content: "Soon shall thy arm, unconquer'd steam! afar. Drag the slow barge, or drive the rapid car; Or on wide-waving wings extended bear. The flying chariot through the field of air.
+
+Anyone who should see in the sky such a globe should be aware that, far from being an alarming phenomenon, it is only a machine made of taffetas or light canvas covered with paper, that cannot possibly cause any harm, and which will someday prove serviceable to the wants of society.
+
+At first we will only skim the surface of the earth like young starlings, but soon, emboldened by practice and experience, we will spring into the air with the impetuousness of the eagle, diverting ourselves by watching the childish behavior of the little men or awling miserably around on the earth below us.", question_author: "Jim Doe")
+Question.create(question_title: "The Art of Flying is but newly invented, 'twill improve by degrees, and in time grow perfect; then we may fly as far as the Moon.", question_content: "Why a test question?", question_author: "John Doe")
+Question.create(question_title: "When a prang seems inevitable, endeavor to strike the softest, cheapest object in the vicinity, as slowly and gently as possible.", question_content: "How a test question?", question_author: "Jane Doe")
